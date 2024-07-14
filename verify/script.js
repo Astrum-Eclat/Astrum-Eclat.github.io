25b044494ad9f2ac14f0840bfe63b502124953ab
sambhavmahajan <sambhav_mahajan@outlook.com>
2024-07-14 17:38:37 +0530

    Update Certificates

--- verify/script.js
+++ verify/script.js
@@ -1,5 +1,8 @@
 const downloadLinks = {
-
+    'astrum120247A' : 'https://drive.google.com/file/d/1d86FdIctGUu7hiFGG2mMQXzu4bV-2MkJ/view?usp=sharing',
+    'astrum120247B' : 'https://drive.google.com/file/d/1BeukvAISwCii96R-hg93FWIsWM4AdkQi/view?usp=sharing',
+    'astrum120247C' : 'https://drive.google.com/file/d/1_LAN-SvNzQBqqsjDwUvLGvaaoZ0rwK1i/view?usp=sharing',
+    'astrum120247D' : 'https://drive.google.com/file/d/1amk6fzbvfkzdCINmKWk-3v6x5QJ7LPQu/view?usp=sharing'
 };
 
 function download() {
